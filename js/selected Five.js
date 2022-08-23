c0084405aa4842625aec926cb1a81cbd1dc1299f
unknown <afia.nasrin3e@gmail.com>
2022-08-23 18:27:59 +0600

    I added the player name appendchild

--- js/selected Five.js
+++ js/selected Five.js
@@ -2,12 +2,30 @@ const selectAllButton = document.querySelectorAll(".select-button");
 for (select of selectAllButton) {
   select.addEventListener("click", function (event) {
     event.target.disabled = true;
-    event.target.style.background = "blue";
+    event.target.style.background = "orange";
     const name = event.target.parentNode.parentNode.children[0].innerText;
     const playerObject = {
-      playername: name,
+      playerName: name,
     };
     nameArray.push(playerObject);
     selectPlayer(nameArray);
   });
+}
+const nameArray = [];
+function selectPlayer() {
+  const playerAdded = document.getElementById("player-added");
+  playerAdded.innerHTML = "";
+  for (let i = 0; i < array.length; i++) {
+    const name = nameArray[i].playerName;
+    const playerNumber = i;
+    if (i < 5) {
+      const tr = document.createElement("tr");
+      tr.HTML = `<li>${name}</li>`;
+      playerAdded.appendChild(tr);
+    } else {
+      alert("You cannot add more than 5 player");
+      return playerNumber;
+    }
+    document.getElementById;
+  }
 }
